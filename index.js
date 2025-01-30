c08ad2a2fb16617091063c586afb4e8ab4fb27cc
“Joussepe <76943911@continental.edu.pe>
2025-01-30 10:13:52 -0500

    Se añadio Morgan

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const http = require('http');
+// index.js
+const express = require('express');
+const morgan = require('morgan');
 
-const server = http.createServer((req, res) => {
-    const { url } = req;
-    console.log(url);
-    if(url === '/') {
-      res.end('Hello World');
-    } else if (url === '/contact') {
-      res.end('The Contact Page');
-    } else if (url === '/about') {
-      res.end('The About Page');
-    } else {
-      res.writeHead(404)
-      res.end('Not Found');
-    }
-  });  
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello From Node.js');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('The Contact Page');
+});
+
+app.get('/about', (req, res) => {
+  res.send('The About Page');
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('Not Found');
+});
 
 const PORT = 3000;
 
-server.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
